124175fccd16739694b79c1714930d69ab6edb04
MasterFlomaster <Flomaste.Maste@gmail.com>
2015-10-19 18:04:17 +0300

    Hello, World!

---
+++ JavaSnake/src/javasnake/JavaSnake.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasnake;
+
+/**
+ **
+ * @author Master Flomaster
+ */
+public class JavaSnake {
+
+    
+    public static void main(String[] args) {
+        //  Печатает Здрасьте, всем)
+        System.out.printf ("Hello,World");
+        System.out.println(); 
+    }
+    
+}
